e65f89106ccd5a059c5dd27de8751f1782971382
KT <kuentingshiu@gmail.com>
2014-07-25 22:46:48 -0400

    add Item

--- src/main/java/com/adarp/xiwami/domain/Item.java
+++ src/main/java/com/adarp/xiwami/domain/Item.java
@@ -5,20 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="Item")
 public class Item {
-//    name: DS.attr('string'),
-//    description: DS.attr('string'),
-//    price: DS.attr('number'),
-//    size: DS.attr('string'),
-//    width: DS.attr('number'),
-//    length: DS.attr('number'),
-//    height: DS.attr('number'),
-//    fromAge: DS.attr('number'),
-//    toAge: DS.attr('number'),
-//    condition: DS.attr('string'),
-//    type: DS.attr('string'),
-//    status: DS.attr('string'),
-//    imageUrl: DS.attr('string'),
-//    isDeleted: DS.attr('boolean')
+
 	@Id
 	private String id;
 	
