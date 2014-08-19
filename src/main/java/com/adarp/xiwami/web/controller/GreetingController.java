2acb605479df404fb3c44342d478c8e02a26d094
kuentingshiu <kuentingshiu@gmail.com>
2014-08-19 08:07:54 -0400

    Add HelloWebXml.java to covert WAR without XML files Update wrapper (in build.gradle) to version 2.0 Add index.html as the webpage of localhost:8080/ Remove application ctx at main()

--- src/main/java/com/adarp/xiwami/web/controller/GreetingController.java
+++ src/main/java/com/adarp/xiwami/web/controller/GreetingController.java
@@ -3,6 +3,7 @@ package com.adarp.xiwami.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,4 +15,8 @@ public class GreetingController {
         return "greeting";
     }
 
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
 }
