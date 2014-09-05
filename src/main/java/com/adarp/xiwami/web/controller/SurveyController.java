bfafed4acd31bfeeae9d35cf575ddd3c5d97b0c7
kuentingshiu <kuentingshiu@gmail.com>
2014-09-05 22:12:27 +0800

    add MVC for survey.

--- src/main/java/com/adarp/xiwami/web/controller/SurveyController.java
+++ src/main/java/com/adarp/xiwami/web/controller/SurveyController.java
@@ -1,6 +1,7 @@
 package com.adarp.xiwami.web.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.adarp.xiwami.domain.Survey;
 import com.adarp.xiwami.service.SurveyService;
@@ -28,4 +30,13 @@ public class SurveyController {
 		
 		return responseBody;		
 	}		
+	
+	// View all Surveys
+	@RequestMapping(value = "/surveys", method = RequestMethod.GET, produces = "application/json")
+	public ModelAndView ViewSurvey() {
+		ModelAndView model = new ModelAndView("viewSurveys");
+		List<Survey> surveys = surveyService.viewSurvey();
+		model.addObject("surveys", surveys);
+		return model;		
+	}
 }
