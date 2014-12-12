693fc75861e670c8946f551a336e158c14ae3774
wenlai <wenchenglai@gmail.com>
2014-12-12 15:35:47 -0500

    one more edit

--- src/main/java/com/adarp/xiwami/domain/Tip.java
+++ src/main/java/com/adarp/xiwami/domain/Tip.java
@@ -12,49 +12,60 @@ public class Tip {
 
 	@Id
 	private String id;
-	
+
 	private String user;
 	private String gossipText;
 	private Date createdDate;
 	private List<String> answers = new ArrayList<String>();
-	
-	//The default of the below field is set by backend
+
+	// The default of the below field is set by backend
 	private Boolean isDeleted;
-		
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getUser() {
 		return user;
 	}
+
 	public void setUser(String user) {
 		this.user = user;
 	}
+
 	public String getGossipText() {
 		return gossipText;
 	}
+
 	public void setGossipText(String gossipText) {
 		this.gossipText = gossipText;
 	}
+
 	public Date getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
+
 	public List<String> getAnswers() {
 		return answers;
 	}
+
 	public void setAnswers(List<String> answers) {
 		this.answers = answers;
 	}
+
 	public Boolean getIsDeleted() {
 		return isDeleted;
 	}
+
 	public void setIsDeleted(Boolean isDeleted) {
 		this.isDeleted = isDeleted;
-	}			
+	}
 }
