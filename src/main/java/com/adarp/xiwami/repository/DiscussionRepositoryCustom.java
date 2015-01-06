5e53b438a4ea9322bb31556b03e135ffab146800
kuentingshiu <kuentingshiu@gmail.com>
2015-01-05 22:53:49 -0500

    Fix Discussion/query

--- src/main/java/com/adarp/xiwami/repository/DiscussionRepositoryCustom.java
+++ src/main/java/com/adarp/xiwami/repository/DiscussionRepositoryCustom.java
@@ -6,5 +6,5 @@ import com.adarp.xiwami.domain.Discussion;
 
 public interface DiscussionRepositoryCustom {
 	List<Discussion> query(String creator, String entity, String entityType, String queryText);
-	Discussion save(Discussion newDiscussion);
+	//Discussion save(Discussion newDiscussion);
 }
