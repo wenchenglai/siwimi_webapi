14e2c65a2b66c2d8db2ec32a8ed6c5573c6aea5b
kuentingshiu <kuentingshiu@gmail.com>
2015-02-04 18:16:13 -0500

    implement counts for Tip.

---
+++ src/main/java/com/adarp/xiwami/domain/Count.java
@@ -0,0 +1,22 @@
+package com.adarp.xiwami.domain;
+
+public class Count {
+	
+	private String id;	
+	private int count =0;
+	
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+		
+}
