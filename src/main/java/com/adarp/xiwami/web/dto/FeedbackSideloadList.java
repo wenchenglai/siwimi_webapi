8cb3a0d0f55f8e25b567dc772bddcb689d0c4bb4
wenlai <wenchenglai@gmail.com>
2015-02-08 20:17:23 -0500

    added list of member to FeedbackSideloadList

--- src/main/java/com/adarp/xiwami/web/dto/FeedbackSideloadList.java
+++ src/main/java/com/adarp/xiwami/web/dto/FeedbackSideloadList.java
@@ -3,8 +3,10 @@ package com.adarp.xiwami.web.dto;
 import java.util.List;
 
 import com.adarp.xiwami.domain.Feedback;
+import com.adarp.xiwami.domain.Member;
 
 public class FeedbackSideloadList {
 	public List<Feedback> feedbacks;
 	public List<Feedback> comments;
+	public List<Member> members;
 }
