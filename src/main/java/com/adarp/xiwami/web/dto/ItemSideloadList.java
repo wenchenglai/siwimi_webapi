aa93d4c7693d388c40624c1f52c1b2e4231b5c08
kuentingshiu <kuentingshiu@gmail.com>
2015-03-18 22:30:00 -0400

    add members for querying Activity/Item/Question

---
+++ src/main/java/com/adarp/xiwami/web/dto/ItemSideloadList.java
@@ -0,0 +1,12 @@
+package com.adarp.xiwami.web.dto;
+
+import java.util.List;
+
+import com.adarp.xiwami.domain.Item;
+import com.adarp.xiwami.domain.Member;
+
+public class ItemSideloadList {
+	public List<Item> items;
+	public List<Member> members;
+
+}
---
+++ src/main/java/com/adarp/xiwami/web/dto/QuestionSideloadList.java
@@ -0,0 +1,11 @@
+package com.adarp.xiwami.web.dto;
+
+import java.util.List;
+
+import com.adarp.xiwami.domain.Member;
+import com.adarp.xiwami.domain.Question;
+
+public class QuestionSideloadList {
+	public List<Question> questions;
+	public List<Member> members;
+}
