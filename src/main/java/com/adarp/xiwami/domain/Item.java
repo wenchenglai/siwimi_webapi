38aaaf36fba48eef27fecdfa4209cac1243ba7ff
wenlai <wenchenglai@gmail.com>
2015-03-21 18:32:51 -0400

    zip code fix

--- src/main/java/com/adarp/xiwami/domain/Item.java
+++ src/main/java/com/adarp/xiwami/domain/Item.java
@@ -47,8 +47,10 @@ public class Item {
 
 	// ZipCode stored in MongoDB could only have 4-digit
 	public void setZipCode(String zipCode) {
-		if (Integer.parseInt(zipCode)<10000)
-			zipCode = "0"+zipCode;
+		if (zipCode != null) {
+			if (Integer.parseInt(zipCode)<10000)
+				zipCode = "0"+zipCode;	
+		}
 		this.zipCode = zipCode;
 	}
 
