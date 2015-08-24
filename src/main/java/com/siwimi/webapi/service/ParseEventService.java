6b980fa09a7fb7fd0d6ee5683d47f6228baa2703
kuentingshiu <kuentingshiu@gmail.com>
2015-08-23 21:59:34 -0400

    Fix bugs of image encoding for robot (AADL.org)

--- src/main/java/com/siwimi/webapi/service/ParseEventService.java
+++ src/main/java/com/siwimi/webapi/service/ParseEventService.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,6 +14,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import sun.misc.BASE64Encoder;
 
 import com.siwimi.webapi.domain.Activity;
 import com.siwimi.webapi.domain.Location;
@@ -59,24 +60,37 @@ public class ParseEventService {
 					List<Activity> previous = new ArrayList<Activity>();
 					String[] parts = eventUrl.split("/");
 					String nodeId = parts[2];
-					previous = activityRep.queryActivity("Ann Arbor Distric Library : "+nodeId, null, null, null, null, null, 
-							                             null, null, null, null, null, null, null);
+					if (nodeId!=null)
+						previous = activityRep.queryActivity("Ann Arbor Distric Library : "+nodeId, null, null, null, null, null, 
+							                                  null, null, null, null, null, null, null);
 					// Populate data into activity object	
 					if ((title!=null) && !title.isEmpty() && previous.isEmpty() && (nodeId!=null)) {
 						Activity activity = new Activity();
 						// Populate activity : creator Id. We need this ensure no duplicated activities posted by robot
 						activity.setCreator("Ann Arbor Distric Library : "+nodeId);
 						// Populate activity : url
-						activity.setUrl("http://www.aadl.org"+eventUrl);
+						if (eventUrl!=null)
+							activity.setUrl("http://www.aadl.org"+eventUrl);
 						// Populate activity : title
 						activity.setTitle(title);
-						String imageUrl = e.select("img").attr("src");						
-						Response resultImageResponse = Jsoup.connect(imageUrl).ignoreContentType(true).execute();
-						String imageData = Base64.encodeBase64URLSafeString(resultImageResponse.bodyAsBytes());
-						// Populate activity : imageUrl
-						activity.setImageUrl(imageUrl);
-						// Populate activity : imageData
-						activity.setImageData(imageData);
+						// Populate created date
+						activity.setCreatedDate(new Date());
+						// Populate activity : imageUrl and imageData
+						String imageUrl = e.select("img").attr("src");
+						if (imageUrl!=null) {
+							activity.setImageUrl(imageUrl);		
+							// Populate activity : imageData
+							Response resultImageResponse = Jsoup.connect(imageUrl).ignoreContentType(true).execute();
+							BASE64Encoder encoder = new BASE64Encoder();
+							String imageData = encoder.encode(resultImageResponse.bodyAsBytes());
+							//System.out.println(imageData);
+							if (imageUrl.contains(".jpg")) {
+								imageData = "data:image/jpeg;base64," + imageData;
+							} else if (imageUrl.contains(".png")) {
+								imageData = "data:image/png;base64," + imageData;	
+							}
+							activity.setImageData(imageData);
+						}					
 						// Populate activity : description
 						activity.setDescription(content2);
 						if (content1 != null) {
