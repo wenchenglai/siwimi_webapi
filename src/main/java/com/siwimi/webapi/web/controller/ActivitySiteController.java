4b100ff5b8ea2ac0cb0989fc4a7c4a20ae945669
wenchenglai <wenchenglai@gmail.com>
2015-12-30 12:14:02 -0500

    fixed ActivitySite controller bugs

--- src/main/java/com/siwimi/webapi/web/controller/ActivitySiteController.java
+++ src/main/java/com/siwimi/webapi/web/controller/ActivitySiteController.java
@@ -1,6 +1,8 @@
 package com.siwimi.webapi.web.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.siwimi.webapi.domain.ActivitySite;
 import com.siwimi.webapi.repository.ActivitySiteRepository;
+import com.siwimi.webapi.web.dto.ActivitySiteRoot;
 
 
 @RestController
@@ -23,32 +26,45 @@ public class ActivitySiteController {
 	
 	// Get all activity Sites
 	@RequestMapping(value = "/activitySites", method = RequestMethod.GET, produces = "application/json")
-	public List<ActivitySite> getActivitySites() {
-		return activitySiteRep.findAll();
+	public Map<String, List<ActivitySite>> getActivitySites() {
+		Map<String, List<ActivitySite>> responseBody = new HashMap<String, List<ActivitySite>>();
+		responseBody.put("activitySite", activitySiteRep.findAll());
+
+		return responseBody;
 	}
+	
+	// Get site by ID
+	@RequestMapping(value = "/activitySites/{id}", method = RequestMethod.GET, produces = "application/json")
+	public Map<String, ActivitySite> findById(@PathVariable("id") String id) {
+		Map<String, ActivitySite> responseBody = new HashMap<String, ActivitySite>();			
+		responseBody.put("activitySite", activitySiteRep.findOne(id));
+		return responseBody;
+	}	
 
 	// Manually add new activity site
 	@RequestMapping(value = "/activitySites", method = RequestMethod.POST, produces = "application/json")
-	public ActivitySite addActivitySite(@RequestBody ActivitySite newActivitySite) {		
-		return activitySiteRep.save(newActivitySite);		
+	public ActivitySite addActivitySite(@RequestBody ActivitySiteRoot newActivitySite) {		
+		return activitySiteRep.save(newActivitySite.activitySite);		
 	}	
 	
 	// Update Activity site
 	@RequestMapping(value = "/activitySites/{id}", method = RequestMethod.PUT, produces = "application/json")
-	public ActivitySite updateActivitySite(HttpServletResponse response,
+	public Map<String, ActivitySite> updateActivitySite(HttpServletResponse response,
 			                           @PathVariable("id") String id, 
-			                           @RequestBody ActivitySite updatedActivitySite) {
+			                           @RequestBody ActivitySiteRoot updatedActivitySite) {
 		ActivitySite site = activitySiteRep.findOne(id);
 		if (site != null) {
-			updatedActivitySite.setId(id);
+			updatedActivitySite.activitySite.setId(id);
 			response.setStatus(HttpServletResponse.SC_OK);
-			return activitySiteRep.save(updatedActivitySite);
+			ActivitySite as = activitySiteRep.save(updatedActivitySite.activitySite);
+			
+			Map<String, ActivitySite> responseBody = new HashMap<String, ActivitySite>();
+			responseBody.put("activitySite", as);
+			return responseBody;
 		} else {
 			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			return null;			
 		}
-
-			
 	}
 	
 	// Delete Activity site
---
+++ src/main/java/com/siwimi/webapi/web/dto/ActivitySiteRoot.java
@@ -0,0 +1,7 @@
+package com.siwimi.webapi.web.dto;
+
+import com.siwimi.webapi.domain.ActivitySite;
+
+public class ActivitySiteRoot {
+	public ActivitySite activitySite;
+}
