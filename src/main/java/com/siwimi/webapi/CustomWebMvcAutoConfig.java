ae7a44486cc9ecaa1b73f3c7cd3ef9bc0529ce39
wenchenglai <wenchenglai@gmail.com>
2016-01-19 14:26:04 -0500

    test

--- src/main/java/com/siwimi/webapi/CustomWebMvcAutoConfig.java
+++ src/main/java/com/siwimi/webapi/CustomWebMvcAutoConfig.java
@@ -11,8 +11,9 @@ public class CustomWebMvcAutoConfig extends WebMvcConfigurerAdapter {
         	"classpath:/META-INF/resources/", "classpath:/resources/",
         	"classpath:/static/", "classpath:/public/"};
 	
+	//, "file:/home/siwimi/image/events/"
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS_from_file = {
-    	"file:/root/image/events/",  "file:/Users/wen/image/events/", "file:/home/siwimi/image/events/"};
+    	"file:/root/image/events/",  "file:/Users/wen/image/events/"};
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
